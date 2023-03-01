7c732093a10a2f5c0ee51ac651b4c7600d6d714f
= DandyWs <= dandyws71@gmail.com>
2023-03-01 16:48:44 +0700

    Merubah route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ContactUsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +23,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/home', HomeController::class);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('product')->group(function () {
+    Route::get('/product1', [ProductController::class, 'product1']);
+    Route::get('/product2', [ProductController::class, 'product2']);
+    Route::get('/product3', [ProductController::class, 'product3']);
+});
+
+Route::get('/news/{title}', NewsController::class );
+
+Route::prefix('/program')->group(function () {
+    Route::get('/program1', [ProgramController::class, 'program1']);
+    Route::get('/program2', [ProgramController::class, 'program2']);
+    Route::get('/program3', [ProgramController::class, 'program3']);
+});
+
+Route::get('/about-us', AboutUsController::class);
+
+Route::resource('contact-us', ContactUsController::class)->only(['index', 'store']);
